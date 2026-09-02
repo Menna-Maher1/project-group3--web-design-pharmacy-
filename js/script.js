e7769fcd7339514204d84e2f4ae6e8bbeb41e95f
Menna Maher <https://github.com/Menna-Maher1>
2026-09-02 15:15:27 +0300

    project

--- js/script.js
+++ js/script.js
@@ -1,10 +1,28 @@
-// Category
-  const categoryCollapse = document.getElementById('categoryCollapse');
-  const categoryIcon = document.getElementById('categoryIcon');
+var categories = document.querySelectorAll(".category-title");
 
-  categoryCollapse.addEventListener('show.bs.collapse', () => {
-    categoryIcon.style.transform = 'rotate(0deg)';
-  });
-  categoryCollapse.addEventListener('hide.bs.collapse', () => {
-    categoryIcon.style.transform = 'rotate(-90deg)';
-  });
+categories.forEach(function(category) {
+
+    category.addEventListener("click", function() {
+
+        var options = category.nextElementSibling;
+        var arrow = category.querySelector(".arrow i");
+
+        if (options.style.display === "block") {
+
+            options.style.display = "none";
+
+            arrow.classList.remove("fa-chevron-down");
+            arrow.classList.add("fa-chevron-right");
+
+        } else {
+
+            options.style.display = "block";
+
+            arrow.classList.remove("fa-chevron-right");
+            arrow.classList.add("fa-chevron-down");
+
+        }
+
+    });
+
+});
